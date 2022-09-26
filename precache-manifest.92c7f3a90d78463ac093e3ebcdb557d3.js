11f5d325a2d430bea43a1643f6d957b16ffd6f9d
jwernethUMD <jason60035@gmail.com>
2022-09-26 17:08:22 -0400

    Updates

---
+++ precache-manifest.92c7f3a90d78463ac093e3ebcdb557d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "add40e8cf08142da0a5a",
+    "url": "/custom-chess/static/css/main.ea857172.chunk.css"
+  },
+  {
+    "revision": "add40e8cf08142da0a5a",
+    "url": "/custom-chess/static/js/main.add40e8c.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/custom-chess/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "b8f4d527d172a93ce8d7",
+    "url": "/custom-chess/static/js/2.b8f4d527.chunk.js"
+  },
+  {
+    "revision": "607f9ebbec7bb71aacb6",
+    "url": "/custom-chess/static/js/runtime~main.607f9ebb.js"
+  },
+  {
+    "revision": "998853d83bf230835dab104c6d0137fe",
+    "url": "/custom-chess/index.html"
+  }
+];
